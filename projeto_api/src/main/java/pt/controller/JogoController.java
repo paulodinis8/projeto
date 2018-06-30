4036e09f0f1645a62ce0dd8ecad38e9e83cad8fa
nelson <nelson.emanuel.p.torres@gmail.com>
2018-06-30 15:38:34 +0100

    list of games -> page of games

--- projeto_api/src/main/java/pt/controller/JogoController.java
+++ projeto_api/src/main/java/pt/controller/JogoController.java
@@ -1,6 +1,8 @@
 package pt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import pt.domain.Jogo;
 import pt.repository.JogoRespository;
@@ -15,9 +17,15 @@ public class JogoController {
     JogoRespository jogoRespository;
 
     @CrossOrigin
-    @GetMapping("/jogo")
+    @GetMapping("/jogoTodos")
     public  List<Jogo> index(){
         return jogoRespository.findAll();
+    }
+    
+    @CrossOrigin
+    @GetMapping("/jogo")
+    public Page<Jogo> findByPage(@RequestParam("page")int  page, @RequestParam("size") int size ){
+    	return jogoRespository.findAllPaginated(new PageRequest(page,size));
     }
 
     @CrossOrigin
--- projeto_api/src/main/java/pt/repository/JogoRespository.java
+++ projeto_api/src/main/java/pt/repository/JogoRespository.java
@@ -1,12 +1,16 @@
 package pt.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import pt.domain.*;
-
-import java.util.List;
+import pt.domain.Jogo;
 
 @Repository
 public interface JogoRespository extends JpaRepository<Jogo, Integer>{
     //List<Jogo> findByNome(String nome);
+
+	@Query("Select j from Jogo j")
+	Page<Jogo> findAllPaginated(Pageable pageable);
 }
