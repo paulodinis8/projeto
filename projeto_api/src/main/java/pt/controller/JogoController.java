112391c1f641720952deb2a1371cf7827437bba0
nelson <nelson.emanuel.p.torres@gmail.com>
2018-07-02 12:49:34 +0100

    switched jogo postendpoint

--- projeto_api/src/main/java/pt/controller/JogoController.java
+++ projeto_api/src/main/java/pt/controller/JogoController.java
@@ -1,21 +1,33 @@
 package pt.controller;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import pt.domain.Jogo;
+import pt.repository.CategoriaRespository;
 import pt.repository.JogoRespository;
 
 import java.util.List;
 import java.util.Map;
 
+
 @RestController
 public class JogoController {
 
+    Gson gson = new Gson();
+    
+    
     @Autowired
     JogoRespository jogoRespository;
-
+    
+    @Autowired
+    CategoriaRespository categoriaRespository;
+    
+    
+    
+    
     @CrossOrigin
     @GetMapping("/jogoTodos")
     public  List<Jogo> index(){
@@ -42,12 +54,9 @@ public class JogoController {
     }*/
 
     @PostMapping("/jogo")
-    public Jogo create(@RequestBody Map<String, String> body){
-        String nome = body.get("nome");
-        String link = body.get("link");
-        String descricao = body.get("descricao");
-        String foto=body.get("foto");
-        return jogoRespository.save(new Jogo(nome, link, descricao, foto));
+    public Jogo create(@RequestBody Jogo jogo){
+        
+        return jogoRespository.save(jogo);
     }
 
 
